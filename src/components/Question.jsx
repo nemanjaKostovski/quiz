99a849d3cccaa4a8c904d4c7610228f221865f3d
Nemanja <nemanjakostovski@gmail.com>
2023-07-31 08:37:06 +0200

    Minor change

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -12,17 +12,16 @@ const Question = ({
       {options.map((option) => {
         return (
           <div key={option}>
-            <span onClick={enableNext}>
-              <input
-                type='radio'
-                name={question}
-                id={option}
-                value={option}
-                checked={answer === option}
-                onChange={(e) => onAnswerChange(e.currentTarget.value)}
-              />
-              <label htmlFor={option}>{option}</label>
-            </span>
+            <input
+              type='radio'
+              name={question}
+              id={option}
+              value={option}
+              checked={answer === option}
+              onChange={(e) => onAnswerChange(e.currentTarget.value)}
+              onClick={enableNext}
+            />
+            <label htmlFor={option}>{option}</label>
           </div>
         );
       })}
